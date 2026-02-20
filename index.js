b1fbaed2b158bd656df7a72ba39853349270e2f6
Marcelo Henrique <marcelosoares2005@outlook.com>
2026-02-20 18:24:54 -0300

    V2

--- index.js
+++ index.js
@@ -16,8 +16,65 @@ server.get('/', (requisicao, resposta) => {
     <title>Calculadora de Ajuste de Salário</title>
 </head>
 <body>
-    <h1>Olá, mundo!</h1>
-    <h2>Bem-vindo à Calculadora de Ajuste de Salário</h2>
+    <h2 style="color: black; text-align: center;">Bem-vindo à Calculadora de Ajuste de Salário</h2>
+    <table style="border-collapse: collapse;
+    width: 700px;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    margin: 0 auto;
+    border: 1px solid #ffffff;
+    background-color: #000000;
+    color: yellow;">
+  <tr>
+    <th>Faixa etária</th>
+    <th>Sexo</th>
+    <th>Reajuste</th>
+    <th>Desconto (até 10 anos na empresa)</th>
+    <th>Acréscimo (mais de 10 anos na empresa)</th>
+  </tr>
+
+  <tr>
+    <td rowspan="2">18 – 39</td>
+    <td>M</td>
+    <td>10%</td>
+    <td>R$ 10,00</td>
+    <td>R$ 17,00</td>
+  </tr>
+  <tr>
+    <td>F</td>
+    <td>8%</td>
+    <td>R$ 11,00</td>
+    <td>R$ 16,00</td>
+  </tr>
+
+  <tr>
+    <td rowspan="2">40 – 69</td>
+    <td>M</td>
+    <td>8%</td>
+    <td>R$ 5,00</td>
+    <td>R$ 15,00</td>
+  </tr>
+  <tr>
+    <td>F</td>
+    <td>10%</td>
+    <td>R$ 7,00</td>
+    <td>R$ 14,00</td>
+  </tr>
+
+  <tr>
+    <td rowspan="2">70 – 99</td>
+    <td>M</td>
+    <td>15%</td>
+    <td>R$ 15,00</td>
+    <td>R$ 13,00</td>
+  </tr>
+  <tr>
+    <td>F</td>
+    <td>17%</td>
+    <td>R$ 17,00</td>
+    <td>R$ 12,00</td>
+  </tr>
+</table>
 </body>
 </html>`);
 });
